df8e70682529c88b9759a2e564a8544dccafe303
T-Couture <106276610+T-Couture@users.noreply.github.com>
2022-06-03 10:27:06 -0400

    Update sum_double.py

--- Warmup-1/sum_double.py
+++ Warmup-1/sum_double.py
@@ -1,18 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 # Given two int values, return their sum. Unless the two values are the same, then return double their sum.
 
 # sum_double(1, 2) → 3
 # sum_double(3, 2) → 5
 # sum_double(2, 2) → 8
-
-
-# In[ ]:
-
 
 def sum_double(a, b):
   if a==b:
