4ce52cb660d87fa67b695b4f91b329656412dbf2
T-Couture <106276610+T-Couture@users.noreply.github.com>
2022-06-03 10:37:26 -0400

    Update makes10.py

--- Warmup-1/makes10.py
+++ Warmup-1/makes10.py
@@ -1,18 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 # Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10.
 
 # makes10(9, 10) → True
 # makes10(9, 9) → False
 # makes10(1, 9) → True
-
-
-# In[ ]:
-
 
 def makes10(a, b):
   if a == 10 or b == 10 or a + b == 10:
