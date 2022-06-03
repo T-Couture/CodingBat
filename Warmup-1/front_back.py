cd8be9aa49db21f6ca06806a7d20404d89c348e1
T-Couture <106276610+T-Couture@users.noreply.github.com>
2022-06-03 11:18:14 -0400

    Add files via upload

---
+++ Warmup-1/front_back.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Given a string, return a new string where the first and last chars have been exchanged.
+
+# front_back('code') → 'eodc'
+# front_back('a') → 'a'
+# front_back('ab') → 'ba'
+
+
+# In[ ]:
+
+
+def front_back(str):
+  if len(str) <= 1:
+    return str
+  return str[-1:] + str[1:-1] + str[0:1]
+
