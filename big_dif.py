0e211853a0f81520967f2607ae9d99e1bcb39528
T-Couture <106276610+T-Couture@users.noreply.github.com>
2022-06-03 10:03:17 -0400

    Update big_dif.py

--- big_dif.py
+++ big_dif.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 # Given an array length 1 or more of ints, return the difference between the largest and smallest values in the array. 
 # Note: the built-in min(v1, v2) and max(v1, v2) functions return the smaller or larger of two values.
 
@@ -11,9 +5,6 @@
 # big_diff([10, 3, 5, 6]) → 7
 # big_diff([7, 2, 10, 9]) → 8
 # big_diff([2, 10, 7, 2]) → 8
-
-
-# In[ ]:
 
 
 def big_diff(nums):
