4cf79279cec8681dc529b87515979c33bc2e1eb1
T-Couture <106276610+T-Couture@users.noreply.github.com>
2022-06-03 10:26:43 -0400

    Add files via upload

---
+++ Warmup-1/sum_double.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+
+# sum_double(1, 2) → 3
+# sum_double(3, 2) → 5
+# sum_double(2, 2) → 8
+
+
+# In[ ]:
+
+
+def sum_double(a, b):
+  if a==b:
+    return (a+b)*2
+  else:
+    return a+b
+
