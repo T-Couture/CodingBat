9fbd27daf8e4dd811502b0e34b25ef7e463f27ab
T-Couture <106276610+T-Couture@users.noreply.github.com>
2022-06-03 10:24:42 -0400

    Update monkey_trouble.py

--- Warmup-1/monkey_trouble.py
+++ Warmup-1/monkey_trouble.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 # We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling. 
 # We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.
 
@@ -11,10 +5,6 @@
 # monkey_trouble(True, True) → True
 # monkey_trouble(False, False) → True
 # monkey_trouble(True, False) → False
-
-
-# In[ ]:
-
 
 def monkey_trouble(a_smile, b_smile):
   if a_smile and b_smile:
